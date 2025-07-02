8c4ad9b5e22a322ef99024db4bcabe848aff2723
siri-05 <sirishasaragadam43@gmail.com>
2025-07-02 11:58:57 +0530

    Python

--- Class Work/4.Operators.py
+++ Class Work/4.Operators.py
@@ -4,6 +4,27 @@ b=10
 print("**Arithmetic Operations on a and b**\nAddition: ",a+b,"\nSubtraction: ",a-b,"\nMultiplication: ",a*b,
       "\nDivision: ",a/b,"\nFloor Division: ",a//b,"\nModulus: ",a%b,"\nExponentiation: ",a**b)
 
-#Comparison Opearators
-print("**Comparison Operations on a and b**\nEqual to: ",a==b,"\nNot equal to: ",a!=b,"\nGreater than: ",a>b,"\nLess than: ",a<b,
+#Comparison Operators
+print("**\nComparison Operations on a and b**\nEqual to: ",a==b,"\nNot equal to: ",a!=b,"\nGreater than: ",a>b,"\nLess than: ",a<b,
       "\nGreater than or equal to: ",a>=b,"\nLess than or equal to: ",a<=b)
+
+#Assignment Operators
+a=10 #assign
+print("\n**Assignment Operations on a and b**\nAssign: ",a)
+a+=10
+print("Add & assign: ",a)
+a-=10
+print("Subtract & assign: ",a)
+a*=4
+print("Multiplication & assign: ",a)
+a/=4
+print("Divison & assign: ",a)
+a=10 #reassign
+a//=2
+print("Floor divison & assign: ",a)
+a%=3
+print("Modulus & assign: ",a)
+a**=3
+print("Exponentiate & assign: ",a)
+
+#Logical Operators

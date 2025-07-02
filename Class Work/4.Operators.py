290728bd62a2a461ec9fb06c7953ba6524b39b40
siri-05 <sirishasaragadam43@gmail.com>
2025-07-02 11:39:32 +0530

    Python

--- Class Work/4.Operators.py
+++ Class Work/4.Operators.py
@@ -1,5 +1,9 @@
 #Arithmetic Operators
 a=20
 b=10
-print("Arithmetic Operations on a and b\nAddition: ",a+b,"\nSubtraction: ",a-b,"\nMultiplication: ",a*b,
+print("**Arithmetic Operations on a and b**\nAddition: ",a+b,"\nSubtraction: ",a-b,"\nMultiplication: ",a*b,
       "\nDivision: ",a/b,"\nFloor Division: ",a//b,"\nModulus: ",a%b,"\nExponentiation: ",a**b)
+
+#Comparison Opearators
+print("**Comparison Operations on a and b**\nEqual to: ",a==b,"\nNot equal to: ",a!=b,"\nGreater than: ",a>b,"\nLess than: ",a<b,
+      "\nGreater than or equal to: ",a>=b,"\nLess than or equal to: ",a<=b)

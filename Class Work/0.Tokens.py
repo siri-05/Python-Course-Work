cea8d0b0ffd73d97c443be2eef578be31b8e73bd
siri-05 <sirishasaragadam43@gmail.com>
2025-07-02 11:44:59 +0530

    Python

---
+++ Class Work/0.Tokens.py
@@ -0,0 +1,22 @@
+#Single line comment
+'''Multiline comment'''
+num=12
+if num%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
+'''
+num=identifier
+=operator
+12=literal
+if=keyword
+%=operator
+2=literal
+== =operator
+0=literal
+:=puntuators
+print=identifier
+even=literal
+else=keyword
+odd=literal
+'''

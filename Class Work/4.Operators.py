2d53144530c331f87b52d276bc6a437bf1a88efb
siri-05 <sirishasaragadam43@gmail.com>
2025-07-02 11:24:05 +0530

    Python

---
+++ Class Work/4.Operators.py
@@ -0,0 +1,5 @@
+#Arithmetic Operators
+a=20
+b=10
+print("Arithmetic Operations on a and b\nAddition: ",a+b,"\nSubtraction: ",a-b,"\nMultiplication: ",a*b,
+      "\nDivision: ",a/b,"\nFloor Division: ",a//b,"\nModulus: ",a%b,"\nExponentiation: ",a**b)

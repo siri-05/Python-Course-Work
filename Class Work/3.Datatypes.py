14d391278fd75a68846321ded5eab2a9e79d0057
siri-05 <sirishasaragadam43@gmail.com>
2025-07-03 11:37:22 +0530

    Python

---
+++ Class Work/3.Datatypes.py
@@ -0,0 +1,40 @@
+a=10
+print("a: ",a," - ",type(a))
+a=10.0
+print("a: ",a," - ",type(a))
+a=3+3j
+print("a: ",a," - ",type(a))
+a='siri'
+print("a: ",a," - ",type(a))
+a="siri"
+print("a: ",a," - ",type(a))
+a='''siri'''
+print("a: ",a," - ",type(a))
+a=''
+print("a: ",a," - ",type(a))
+a=""
+print("a: ",a," - ",type(a))
+a=''''''
+print("a: ",a," - ",type(a))
+a=['siri','hitha','sravani','likitha']
+print("a: ",a," - ",type(a))
+a=[]
+print("a: ",a," - ",type(a))
+a=list()
+print("a: ",a," - ",type(a))
+a=('siri','hitha','sravani','likitha')
+print("a: ",a," - ",type(a))
+a=()
+print("a: ",a," - ",type(a))
+a=tuple()
+print("a: ",a," - ",type(a))
+a={'siri','hitha','sravani','likitha'}
+print("a: ",a," - ",type(a))
+a=set()
+print("a: ",a," - ",type(a))
+a={'Name':'Sirisha','Course':'DS','Batch':'15'}
+print("a: ",a," - ",type(a))
+a={}
+print("a: ",a," - ",type(a))
+a=dict()
+print("a: ",a," - ",type(a))
